b5003461ce5bcea8452914c80b8f9d494b34e4b4
SumanaiEugen <ryanlim943@gmail.com>
2022-01-13 10:17:14 +0800

    changed falling object speed
    changed the falling object speed from -4 to -7

--- 03_RyanLimkokCheng_Lab03/Assets/Scripts/ObjectMovement.cs
+++ 03_RyanLimkokCheng_Lab03/Assets/Scripts/ObjectMovement.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ObjectMovement : MonoBehaviour
 {
-    private float ySpeed = -4f;
+    private float ySpeed = -7f;
     // Start is called before the first frame update
     void Start()
     {
