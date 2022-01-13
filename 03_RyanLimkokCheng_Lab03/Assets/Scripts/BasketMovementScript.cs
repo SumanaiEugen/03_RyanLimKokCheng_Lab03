e83e4905d969f8765195d108322718c9d096b51f
SumanaiEugen <ryanlim943@gmail.com>
2022-01-13 09:48:05 +0800

    updated Basket CS
    updated with collision between healthy and unhealthy food

--- 03_RyanLimkokCheng_Lab03/Assets/Scripts/BasketMovementScript.cs
+++ 03_RyanLimkokCheng_Lab03/Assets/Scripts/BasketMovementScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BasketMovementScript : MonoBehaviour
 {
     public float speed;
-   
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +17,23 @@ public class BasketMovementScript : MonoBehaviour
     {
 
       float horizontalInput = Input.GetAxis("Horizontal");
-
+    
       transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
-
-        
-       
 
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Healthy"))
+        {
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Unhealthy"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 
 
-    
 
 }
